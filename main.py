7545a2e574a76481a1e7e22fef3240f69d74ea96
Kostya Hostiuk <kostelogostiuk98@gmail.com>
2026-03-25 11:10:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("safe_hromada")
+import telebot
+from gevent.testing.travis import command
+from telebot import types
+from a_key import api
+api = api
+bot = telebot.TeleBot(api)
+@bot.message_handlers(commands=['start'])
+def start(message):
+    bot.send_message(message.from_user.id, "Даскаво просимо")
