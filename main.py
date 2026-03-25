9832c10a871c0929af5c4d4cfb8aad92888ba780
Kostya Hostiuk <kostelogostiuk98@gmail.com>
2026-03-25 21:04:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,17 @@ def start(message):
     but2 = types.KeyboardButton('Скарги')
     but3 = types.KeyboardButton('Пропозиції')
     markup.add(but1,but2,but3)
-    bot.send_message(message.from_user.id, f'Вас вітає чат-бот {chr(39)}{chr(39)}Безпечна громада{chr(39)}{chr(39)}' 
+    bot.send_message(message.from_user.id, f'Вас вітає чат-бот {chr(39)}{chr(39)}Безпечна громада{chr(39)}{chr(39)}'
                                            f'Тут ви можете написати звернення, скарги тощо', reply_markup=markup)
+@bot.message_handler(content_types=['text'])
+def text_message(message):
+    end_message = ""
+    if message.text == 'Звернення':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        but4 = types.KeyboardButton('Записати')
+        but5 = types.KeyboardButton('Статус')
+        markup.add(but4,but5)
+        bot.send_message(message.from_user.id, ' ', reply_markup=markup)
+
 
 bot.polling(non_stop=True, interval=0)
