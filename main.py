9cfa9ce37d8b407b6853cc7be267a553687cd9c9
Kostya Hostiuk <kostelogostiuk98@gmail.com>
2026-03-25 13:12:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from telebot import types
 from a_key import api
 api = api
 bot = telebot.TeleBot(api)
-@bot.message_handlers(commands=['start'])
+@bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.from_user.id, "Даскаво просимо")
+
+    bot.send_message(message.from_user.id, f'Вас вітає чат-бот {chr(39)}{chr(39)}Безпечна громада{chr(39)}{chr(39)},'
+                                           f'Тут ви можете написати звернення, скарги тощо')
+
+bot.polling(non_stop=True, interval=0)
