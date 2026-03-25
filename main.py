774dc0e8eac18e46463c62298c58a78d5e8bbcd2
Kostya Hostiuk <kostelogostiuk98@gmail.com>
2026-03-25 19:22:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from telebot import types
 from a_key import api
 api = api
 bot = telebot.TeleBot(api)
-@bot.message_handler(commands=['start'])
+
+@bot.message_handler(commands=['/start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     but1 = types.KeyboardButton('Звернення')
@@ -12,4 +13,5 @@ def start(message):
     markup.add(but1,but2,but3)
     bot.send_message(message.from_user.id, f'Вас вітає чат-бот {chr(39)}{chr(39)}Безпечна громада{chr(39)}{chr(39)},'
                                            f'Тут ви можете написати звернення, скарги тощо', reply_markup=markup)
+
 bot.polling(non_stop=True, interval=0)
